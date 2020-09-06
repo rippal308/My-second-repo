a32e1aaa8d2596fb68e82d93fc1a76925893d3de
rippal308 <rippal999@gmail.com>
2020-09-06 05:45:46 -0400

    added program i guessinggame.sh

---
+++ guessinggame.sh
@@ -0,0 +1,16 @@
+#gussinggame.sh
+
+echo "How many files are in the current directory?"
+
+if in ($i=7)
+echo "congratualtion! Your guess $i is right"
+
+elif in ($i>7)
+echo "your guess is $i which is too high "
+
+else in ($i<7)
+echo "your guess is $i which is too low "
+
+fi
+
+echo "end programm"
